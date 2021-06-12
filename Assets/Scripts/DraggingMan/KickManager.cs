3fc6609f6d40398bcef3274737e20512eaf658c7
MarcWerk <47601591+DrOffensive@users.noreply.github.com>
2021-06-12 22:25:59 +0200

    Added "kick" mechanic

---
+++ Assets/Scripts/DraggingMan/KickManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class KickManager : MonoBehaviour
+{
+
+    Rigidbody Rigidbody => GetComponent<Rigidbody>();
+    [SerializeField] Transform doggo;
+    [SerializeField] Leg leftLeg, rightLeg;
+    [SerializeField] float kickForce;
+    [SerializeField] float artificialForce;
+
+    [System.Serializable]
+    public struct Leg
+    {
+        public HingeJoint thigh, calf;
+    }
+
+    private void Start()
+    {
+        leftLeg.thigh.useMotor = false;
+        rightLeg.thigh.useMotor = false;
+        leftLeg.calf.useMotor = false;
+        rightLeg.calf.useMotor = false;
+
+
+    }
+
+    private void Update()
+    {
+        if (InputManager.GetLeftRightAxis() < 0)
+            Kick(true);
+        else if(InputManager.GetLeftRightAxis() < 0)
+            Kick(false);
+        else
+        {
+            leftLeg.thigh.useMotor = false;
+            rightLeg.thigh.useMotor = false;
+            leftLeg.calf.useMotor = false;
+            rightLeg.calf.useMotor = false;
+        }
+    }
+
+    void Kick (bool left)
+    {
+        if (left)
+        {
+            Rigidbody.AddForce(doggo.TransformDirection(Vector3.left) * artificialForce);
+            leftLeg.thigh.useMotor = true;
+            rightLeg.thigh.useMotor = false;
+            var motor = leftLeg.thigh.motor;
+            //motor.targetVelocity = kickForce;
+            motor.force = kickForce;
+            leftLeg.thigh.motor = motor;
+            leftLeg.calf.useMotor = true;
+            rightLeg.calf.useMotor = false;
+            motor = leftLeg.calf.motor;
+            //motor.targetVelocity = -kickForce;
+            motor.force = -kickForce;
+            leftLeg.calf.motor = motor;
+        }
+        else
+        {
+            Rigidbody.AddForce(doggo.TransformDirection(Vector3.right) * artificialForce);
+            leftLeg.thigh.useMotor = false;
+            rightLeg.thigh.useMotor = true;
+            var motor = rightLeg.thigh.motor;
+            //motor.targetVelocity = kickForce;
+            motor.force = kickForce;
+            rightLeg.thigh.motor = motor;
+            leftLeg.calf.useMotor = false;
+            rightLeg.calf.useMotor = true;
+            motor = rightLeg.calf.motor;
+            //motor.targetVelocity = -kickForce;
+            motor.force = -kickForce;
+            rightLeg.calf.motor = motor;
+        }
+    }
+}
--- Assets/Scripts/Leash.cs
+++ Assets/Scripts/Leash.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Leash : MonoBehaviour
 {
+
+    LineRenderer LineRenderer => GetComponent<LineRenderer>();
     Rigidbody Rigidbody => GetComponent<Rigidbody>();
     [SerializeField] Transform target;
     [SerializeField] float distance;
     [SerializeField] float smoothDampTime = 1f;
     Vector3 targetPosition = Vector3.zero;
     Vector3 currentVelocity = Vector3.zero;
+
+    private void Start()
+    {
+        LineRenderer.positionCount = 2;
+    }
 
     private void FixedUpdate()
     {
@@ -20,5 +28,8 @@ public class Leash : MonoBehaviour
             targetPosition = target.position + (line.normalized * distance);
             Rigidbody.MovePosition(Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothDampTime));
         }
+
+        LineRenderer.SetPosition(0, target.position);
+        LineRenderer.SetPosition(1, transform.position);
     }
 }
