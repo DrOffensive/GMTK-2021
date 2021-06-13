217423438df74fb1732cf31e53eb9b0fd131daf5
MarcWerk <47601591+DrOffensive@users.noreply.github.com>
2021-06-13 14:18:54 +0200

    Made doggo look the right way

--- Assets/Scripts/DraggingMan/FollowSpline.cs
+++ Assets/Scripts/DraggingMan/FollowSpline.cs
@@ -7,7 +7,7 @@ public class FollowSpline : MonoBehaviour
 {
 
     [SerializeField] CurrentTrack track;
-
+    [SerializeField] Transform doggo;
     [SerializeField] float speed;
 
     float flatSpeed = 0f;
@@ -31,6 +31,7 @@ public class FollowSpline : MonoBehaviour
         var pointOnSpline = track.spline.GetPointOnSpline(Mathf.Clamp01(track.time));
         transform.position = pointOnSpline.point;
         transform.rotation = Quaternion.LookRotation(track.spline.Tangent(track.time) * (track.spline.BezierType == BezierType.Quadratic ? -1 : 1));
+        doggo.rotation = Quaternion.LookRotation(transform.right);
     }
 
     // Update is called once per frame
